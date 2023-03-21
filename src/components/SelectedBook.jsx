dec527b84021cd672eda84224e17c6993d106e3f
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-21 12:09:16 +0900

    can make image larger if larger version exists

--- src/components/SelectedBook.jsx
+++ src/components/SelectedBook.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 
 const SelectedBook = (props) => {
   const { bookObject, setSelectedBookObject} = props;
   const handleClick = () => {
     setSelectedBookObject(bookObject);
   };
+  const [url, setUrl] = useState(bookObject.thumbnail);
+  const makeImageSmall = () => {
+    bookObject.thumbnail = bookObject.thumbnail.replace("zoom=0", "zoom=1" );
+    setUrl(bookObject.thumbnail)
+  };
+  const makeImageLarge = () => {
+    bookObject["thumbnail"] = bookObject.thumbnail.replace("zoom=1", "zoom=0" );
+    setUrl(bookObject.thumbnail)
+
+  };
   console.log(bookObject)
   return (
     <div>
+      <div>
+        <button onClick={makeImageSmall}>small image</button>
+        <button onClick={makeImageLarge}>large image</button>
+      </div>
       <img src={bookObject.thumbnail} alt="a book" className='img-fluid selected-book-actual' onClick={handleClick}/>
       <h2>{bookObject.title}</h2>
       <p>{bookObject.description}</p>
